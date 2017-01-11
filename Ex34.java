d9bad5ffc39839067c775f1abd4b5e2455d5e6f5
Romain Scohy <frenchscapegoat@users.noreply.github.com>
2017-01-11 17:04:13 +0100

    Add files via upload
    premiere version de l'exo 3.4

---
+++ Ex34.java
@@ -0,0 +1,6 @@
+// https://github.com/oliviercailloux/java-course/blob/master/Course%20Object/Planning.adoc
+public class Ex34 {
+	public static void main(String[] args){
+		System.out.println("Hello !");
+	}
+}
