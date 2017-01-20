69360415c7bd7d4a067fb660ef179b95c3b66a89
Romain Scohy <frenchscapegoat@users.noreply.github.com>
2017-01-20 20:07:48 +0100

    Delete Ex34.java

--- Ex34.java
+++
@@ -1,6 +0,0 @@
-// https://github.com/oliviercailloux/java-course/blob/master/Course%20Object/Planning.adoc
-public class Ex34 {
-	public static void main(String[] args){
-		System.out.println("Hello !");
-	}
-}
