b6abaa3c6032ee9e87933a767e0dfc1dfb774782
Romano <romain.scohy@dauphine.eu>
2017-01-20 17:12:24 +0100

    Reflexion exo 3.4

--- ListeExercicesJavaApp/src/S1/Ex34.java
+++ ListeExercicesJavaApp/src/S1/Ex34.java
@@ -1,8 +1,13 @@
 package S1;
-
+// http://math.hws.edu/eck/cs124/javanotes7/c3/exercises.html
 // https://github.com/oliviercailloux/java-course/blob/master/Course%20Object/Planning.adoc
 public class Ex34 {
 	public static void main(String[] args){
-		System.out.println("Hello !");
+		/* Idée 
+            On va d'abord récupérer le string avec un scanner
+            Puis on va parcours chaque lettre avec charAt
+            Qunad on tombe sur une lettre, on l'affiche avec print
+            
+            */
 	}
 }
