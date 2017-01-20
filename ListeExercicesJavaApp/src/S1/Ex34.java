f955505b7d12cef9097edbc85cf1bad1a35a41a9
scapegoat <Romano@192.168.1.86>
2017-01-20 16:56:24 +0100

    Envoi template

---
+++ ListeExercicesJavaApp/src/S1/Ex34.java
@@ -0,0 +1,8 @@
+package S1;
+
+// https://github.com/oliviercailloux/java-course/blob/master/Course%20Object/Planning.adoc
+public class Ex34 {
+	public static void main(String[] args){
+		System.out.println("Hello !");
+	}
+}
