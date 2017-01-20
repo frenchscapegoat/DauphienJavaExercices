5f72d17874d9b272b7cdfeb55828b726789d3bd0
Romano <romain.scohy@dauphine.eu>
2017-01-20 17:33:44 +0100

    Début code

--- ListeExercicesJavaApp/src/S1/Ex34.java
+++ ListeExercicesJavaApp/src/S1/Ex34.java
@@ -1,13 +1,24 @@
 package S1;
 // http://math.hws.edu/eck/cs124/javanotes7/c3/exercises.html
 // https://github.com/oliviercailloux/java-course/blob/master/Course%20Object/Planning.adoc
+
+import java.util.Scanner;
+
 public class Ex34 {
 	public static void main(String[] args){
+            
 		/* Idée 
             On va d'abord récupérer le string avec un scanner
             Puis on va parcours chaque lettre avec charAt
-            Qunad on tombe sur une lettre, on l'affiche avec print
-            
+            Quand on tombe sur une lettre (d'après enoncé, on peut utiliser isLetter()), on l'affiche avec print
+            Pas println sinon CR à chaque lettre
+            Quand on tombe sur un espace, on fait un println
             */
+            String str;
+            Scanner sc;
+            sc = new Scanner(System.in);
+            System.out.println("Svp, rentrez une phrase ");
+            
+                
 	}
 }
