56d126b742242bbd9aafc5ede084161ef0af265a
Romano <romain.scohy@dauphine.eu>
2017-02-10 13:03:37 +0100

    Fini

--- S3/src/ex57/SubstractionQuestion.java
+++ S3/src/ex57/SubstractionQuestion.java
@@ -14,8 +14,10 @@ public class SubstractionQuestion implements IntQuestion{
     private int a, b;  // The numbers in the problem.
 
     public SubstractionQuestion() { // constructor
+        do{
         a = (int)(Math.random() * 50 + 1);
         b = (int)(Math.random() * 50);
+        }while(((a-b)<0) || (a-b)==0);
     }
 
     public String getQuestion() {
