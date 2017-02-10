857f5c4ccd20f3708f68d373431b55252c038a78
Romano <romain.scohy@dauphine.eu>
2017-02-10 16:29:48 +0100

    Il manque la boucle qui demande à l'utilisateur s'il est ok pour continuer

--- S4/src/ex54/source/ex54.java
+++ S4/src/ex54/source/ex54.java
@@ -6,6 +6,9 @@
 package ex54.source;
 
 import ex54.BlackjackHand;
+import ex54.Card;
+import ex54.Deck;
+import java.util.Random;
 
 /**
  *
@@ -20,17 +23,43 @@ public class ex54 {
         
         
         /* 
-        Ensuite on récupère un random int entre 2 et 6
-        On récupère autant de carte du deck que le random vien de nous dire
+        
         On fait un println de la mian (ou des cartes de la main)
         Puis un println de BlackJackHand.getBlackjackValue()
         */
         
         // On crée un objet BlackjackHand ; puis un Deck
         BlackjackHand maMainBlackjackHand = new BlackjackHand();
+        Deck monDeck = new Deck();
+        
         
         // On crée un random entre 1 et 6
-        die1 = (int)(Math.random()*6) + 1;
+        Random r = new Random();
+        final int borneSup = 6;
+        final int borneMin = 2;
+        int maVal = borneMin + r.nextInt(borneSup-borneMin);
+        Card laCarte;
+        
+        // On récupère autant de carte du deck que le random vient de nous dire
+        for (int i=0;i< maVal;i++){
+            if (monDeck.cardsLeft() != 0){
+                laCarte = monDeck.dealCard();   // Vous autorisez à créer des variables de lisibilité ?
+                maMainBlackjackHand.addCard(laCarte);
+            }
+            
+        // On fait un affichage de la main
+        for (int j=0;i<maVal;i++){
+            System.out.println("Ma main : "+ maMainBlackjackHand.getCard(j));
+        }
+        
+        // On affiche le score
+        System.out.println("Score Blackjack : "+ maMainBlackjackHand.getBlackjackValue());
+        }
+        
+        
+        
+        
+        
     }
     
 }
